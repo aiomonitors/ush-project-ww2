1c6b2dce683e8773678de06fa2afe03148edad3a
navr <navrxo@gmail.com>
2019-10-20 18:15:23 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import flask
+from flask import Flask, render_template, jsonify, request, session, make_response, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html'), 200
+
+@app.route('/ww2', methods=['GET'])
+def ww2():
+    return render_template('ww2.html'), 200
+
+@app.route('/current', methods=['GET'])
+def current():
+    return render_template('current.html'), 200
+
+@app.route('/race', methods=['GET'])
+def race():
+    return render_template('arms.html'), 200
+
+@app.route('/sources', methods=['GET'])
+def sources():
+    return render_template('ww2.html'), 200
+app.run(port=2121, debug=True)
---
+++ static/;.js
@@ -0,0 +1,71 @@
+window.onload = () => {
+    var i = 0;
+    var txt = 'Atomic Fission'; /* The text */
+    var speed = 100; /* The speed/duration of the effect in milliseconds */
+  
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("brand").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      } /* else {
+        
+        i = 0
+        document.getElementById("brand").innerHTML = "";
+        wait(1000)
+        typeWriter()
+      } */
+     
+    }
+    typeWriter()
+    checkMode()
+    function wait(ms) {
+      setTimeout(function() {}, ms)
+    }
+  
+  
+    $(document).on('click', '#dark-mode', function () {
+      darkMode();
+      //$("a").css('color', '#F1B379');
+    });
+    $(document).on('click', '#light-mode', function () {
+      lightMode();
+    });
+  }
+  
+  function checkMode() {
+    var x = document.cookie;
+    var cookies = {};
+    x = x.split(';');
+    x.forEach(cookie => {
+      var temp = cookie.split('=');
+      cookies[temp[0]] = temp[1];
+    })
+    var mode = cookies['mode'];
+    console.log(mode);
+    if (mode === 'dark') {
+      darkMode()
+    } else {
+      $("body").css('background-color', 'white');
+      $("body").css('color', 'black');
+      $(".jumbotron").css('color', 'white');
+    }
+  }
+  
+function darkMode () {
+    $("body").css('background-color', '#221E20');
+    $("body").css('color', 'white')
+    $(".jumbotron").css('color', '#F1B379');
+    $('.list-link').css('background-color', '#d7dbdd')
+    $('.list-link').css('color', 'black')
+    document.cookie = "mode=dark"
+}
+
+function lightMode () {
+    $("body").css('background-color', 'white');
+    $("body").css('color', 'black');
+    $(".jumbotron").css('color', 'white');
+    $('.list-link').css('background-color', '#221E20')
+    $('.list-link').css('color', '#F1B379')
+    document.cookie = "mode=light"
+}
