d7580e41ba33d1db705c84ddbf85bad57a0ed097
navr <navrxo@gmail.com>
2019-10-20 20:52:26 -0400

    Done

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-import flask
+import flask, json
 from flask import Flask, render_template, jsonify, request, session, make_response, redirect, url_for
 app = Flask(__name__)
 
+def open_json(jsonfile):
+    try:
+        with open(jsonfile, 'r') as outfile:
+            data = outfile.read()
+            data = json.loads(data)
+            outfile.close()
+            return(data)
+    except:
+        return(None)
+
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html'), 200
+    return render_template('ww2.html'), 200
 
 @app.route('/ww2', methods=['GET'])
 def ww2():
@@ -20,7 +30,8 @@ def race():
 
 @app.route('/sources', methods=['GET'])
 def sources():
-    return render_template('ww2.html'), 200
+    sources = open_json('sources.json')['sources']
+    return render_template('sources.html', sources=sources), 200
 
 if __name__ == '__main__':
     app.run()
